ab0c3d50a1561bf9ff8cda7c972cd31dc95d6584
hector barazorda <126804661+hectorDev2@users.noreply.github.com>
2024-09-25 10:33:11 -0500

    Delete utils directory

--- utils/paintMatrix.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-
-# Función para mostrar la matriz de puntuación
-def print_matrix_with_pandas(score_matrix, seq1, seq2):
-    df = pd.DataFrame(score_matrix, index=[''] + list(seq1), columns=[''] + list(seq2))
-    print(df)
